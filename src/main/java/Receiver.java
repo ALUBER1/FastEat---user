ff5ed5ebe3c919df477d7ddae92f8eaca52efb2a
Giovanni Martinelli <giovava007@gmail.com>
2026-05-17 19:34:07 +0200

    final closes issue 5

--- src/main/java/Receiver.java
+++ src/main/java/Receiver.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+
 import com.google.gson.Gson;
 import models.dto.InsertResponseDto;
 
@@ -17,10 +18,12 @@ public class Receiver extends Thread {
     public Receiver(Storage storage, Socket socket) {
         this.storage = storage;
         this.socket = socket;
+
     }
 
     @Override
     public void run() {
+        Gson gson = new Gson();
         try {
             InputStream istream = socket.getInputStream();
 
@@ -34,10 +37,20 @@ public class Receiver extends Thread {
             while (storage.running()) {
 
                 String json = reader.readLine();
-                Gson gson = new Gson();
-                InsertResponseDto message = gson.fromJson(json, InsertResponseDto.class);
+
+                InsertResponseDto message =
+                        gson.fromJson(json, InsertResponseDto.class);
 
                 storage.addMessage(message.getMsg());
+
+                Util.clearConsole();
+
+                System.out.println(message.getMsg());
+
+                String menu = storage.getMenu();
+                if (menu != null) {
+                    System.out.print(menu);
+                }
             }
 
         } catch (IOException e) {
