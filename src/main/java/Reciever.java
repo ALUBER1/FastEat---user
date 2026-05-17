bbad38238a8a5363433421934d598f727794a85f
ALUBER1 <140241493+ALUBER1@users.noreply.github.com>
2026-05-17 18:42:20 +0200

    Revert "closes issue-5"

--- src/main/java/Reciever.java
+++ src/main/java/Reciever.java
@@ -1,41 +1,8 @@
-import com.google.gson.Gson;
-
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Vector;
-
 public class Reciever extends Thread {
-
-    private final Storage storage;
-    private final Socket socket;
-    private final Gson gson;
-
-    public Reciever(Storage storage, Socket socket) {
-        this.storage = storage;
-        this.socket = socket;
-        this.gson = new Gson();
-    }
+    Storage s;
 
     @Override
     public void run() {
-        try {
-            OutputStream ostream = socket.getOutputStream();
-            BufferedOutputStream writer = new BufferedOutputStream(ostream);
-
-            while (storage.running()) {
-                Vector<String> messages = storage.getMessages();
-
-                for (Object message : messages) {
-                    String json = gson.toJson(message) + "\n";
-                    writer.write(json.getBytes(StandardCharsets.UTF_8));
-                    writer.flush();
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Errore: " + e.getMessage());
-        }
+        
     }
 }
